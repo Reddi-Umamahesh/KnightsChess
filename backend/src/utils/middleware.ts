c8a4dd2c0f24ccd85fd3f4cc8d7563518e815344
Turbobot <turbobot@vercel.com>
2025-01-22 17:54:18 +0000

    feat(create-turbo): apply official-starter transform

--- backend/src/utils/middleware.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-
-export const isAuthenticated = (req: Request, res: Response, next: Function) => {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    res.status(401).json({ message: "Unauthorized" , success:false })
-}
